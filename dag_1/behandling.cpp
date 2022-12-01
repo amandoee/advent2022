5a8bf0b2524d2257be54cf31e7a7a9bed86abc2a
amandoee <amandoee@gmail.com>
2022-12-01 12:18:24 +0100

    dag_1

---
+++ dag_1/behandling.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+    int sum = 0;
+    int maxsum = 0;
+    int maxsum2 = 0;
+    int maxsum3 = 0;
+
+
+
+    int index = 1;
+    int maxindex = 1;
+
+
+
+
+    fstream data;
+
+    data.open("input.txt",ios::in); //open a file to perform read operation using file object
+   if (data.is_open()){ //checking whether the file is open
+      string bd;
+      while(getline(data, bd)){ //read data from file object and put it into string.
+         cout << bd << " "; //print the data of the string
+
+         if (bd == "") {
+            printf("\n");
+            if (sum>maxsum) {maxsum3=maxsum2; maxsum2=maxsum; maxsum=sum;}
+            else if (sum>maxsum2 && sum<=maxsum) {maxsum3=maxsum2; maxsum2=sum;}
+            else if (sum>maxsum3 && sum<=maxsum2) {maxsum3=sum;}
+            index+=1;
+            sum=0;
+         } else {
+            sum=sum+stoi(bd);
+         }
+
+      }
+      cout<<"\nThe three max sums are: "<<maxsum<<" "<<maxsum2<<" "<<maxsum3<<"\n"<<"The total sum is: "<<maxsum+maxsum2+maxsum3<<"\n";
+      }
+
+    return 0;
+}

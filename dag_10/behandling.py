4ee85c7af3256266693bc3a86585b4bfe54448d6
Andreas Hald Søndergaard Mandøe <amandoee@gmail.com>
2022-12-10 17:53:50 +0100

    visuel

--- dag_10/behandling.py
+++ dag_10/behandling.py
@@ -1,11 +1,6 @@
 execution = []
 register = []
-current_execution=[]
-
 signal = []
-counter=1
-
-tekst=[[],[],[],[],[]]
 
 def addx(V):
     execution.append(0)
@@ -20,39 +15,24 @@ with open("input.txt") as input2:
         else:
             execution.append(0)
 
-sum=1
 
-counter2=0
+sum=1
 for i in execution:
-    if counter2==200:
-        print("")
     register.append(i+sum)
     sum+=i
-    counter2+=1
 
-last_executed=0
-for i in execution:
-    if i != 0:
-        current_execution.append(i)
-        last_executed=i
-    else:
-        current_execution.append(last_executed)
 
 
 counter=0
-x=0
 faktor=0
 for i in register:
     #placement er registervrdien.
     if counter==40 or counter==80 or counter==120 or counter==160 or counter==200 or counter==240:
-        x+=1
         faktor+=1
         print(" ")
     if counter==0 or (counter== register[counter-1]+40*faktor or counter==register[counter-1]-1+40*faktor or counter==register[counter-1]+1+40*faktor):
-        #tekst[x].append("#")
         print("█",end="")
     else:
-        #tekst[x].append(".")
         print(" ",end="")
     counter+=1
 
@@ -60,8 +40,6 @@ for i in register:
 counter=1
 for i in register:
     signal.append(i*(counter+1))
-
-
     counter+=1
 
 print(" ")

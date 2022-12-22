df5fd855337d2646dca0bdb3c4508a62f32fc769
amandoee <amandoee@gmail.com>
2022-12-22 21:54:34 +0100

    bruh

--- dag_11/behandling.py
+++ dag_11/behandling.py
@@ -1,153 +1,74 @@
-monkey_handling =[0,0,0,0,0,0,0,0]
-monkey_number=0
-round=0
+class monkey:
+    items = []
+    operation = 0
+    
+    #pointer to the next node
+    t=None
+    f=None
+    
+number=0
+monkeys=list()
 
-def push_monkey(monkey,num):
-    round=0
-    if monkey==0:
-        monkey0(num,round)
-    elif monkey==1:
-        monkey1(num,round)
-    elif monkey==2:
-        monkey2(num,round)
-    elif monkey==3:
-        monkey3(num,round)
-    elif monkey==4:
-        monkey4(num,round)
-    elif monkey==5:
-        monkey5(num,round)
-    elif monkey==6:
-        monkey6(num,round)
-    elif monkey==7:
-        monkey7(num,round)
-    else:
-        print("Error: monkey not found")
+with open("case_input.txt") as input2:
+    for line in input2:
+        monkeys.append(monkey())
+        
+        line=line.strip("\n").split(", ")
+        monkeys[number].items=line
+        number+=1
+input2.close()
 
-
-def monkey0(num,round):
-    num=num*13
-    monkey_handling[0]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%3==0):
-        monkey2(num,round)
-    else:
-        monkey1(num,round)
-
-
-def monkey1(num,round):
-    num=num+2
-    monkey_handling[1]+=1
-    round+=1
-
-    if round==22:
-        return
-    
-    if (num%13==0):
-        monkey7(num,round)
-    else:
-        monkey2(num,round)
-    
-    
-
-def monkey2(num,round):
-    num=num+8
-    monkey_handling[2]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%19==0):
-        monkey4(num,round)
-    else:
-        monkey7(num,round)
-    
-
-def monkey3(num,round):
-    num=num+1
-    monkey_handling[3]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%17==0):
-        monkey6(num,round)
-    else:
-        monkey5(num,round)
-    
-    
-
-def monkey4(num,round):
-    num=num*17
-    monkey_handling[4]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%5==0):
-        monkey6(num,round)
-    else:
-        monkey3(num,round)
-    
-    
-
-def monkey5(num,round):
-    num=num+3
-    monkey_handling[5]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%7==0):
-        monkey1(num,round)
-    else:
-        monkey0(num,round)
-    
-    
-
-def monkey6(num,round):
-    num=num*num
-    monkey_handling[6]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%11==0):
-        monkey5(num,round)
-    else:
-        monkey0(num,round)
-    
-
-def monkey7(num,round):
-    num=num+6
-    monkey_handling[7]+=1
-    round+=1
-    
-    if round==22:
-        return
-    
-    if (num%2==0):
-        monkey4(num,round)
-    else:
-        monkey3(num,round)
-    
-    
-with open("input_v2.txt") as data:
+number=0
+with open("case_operands.txt") as data:
     for line in data:
-        numre = line.strip("\n").split(", ")
-        for num in numre:
-            push_monkey(monkey_number,int(num))
-        monkey_number+=1
+        line=line.split("\n")
+        for i in range(len(line)):
+            monkeys[number].operation=int(line[0])
+        number+=1
+data.close()
 
-print(monkey_handling)
-print(139*130)
+number=0
+with open("case_relations.txt") as data2:
+    for line in data2:
+        line=line[0:line.find("\n")]
+        line=line.split(" ")
+        monkeys[number].t=int(line[0])
+        monkeys[number].f=int(line[1])
+        number+=1
+data2.close()
+
+monkey_inspections=[0,0,0,0,0,0,0,0]
+
+for n in range(20):
+    for i in range(len(monkeys)):
+        monkey_inspections[i]+=len(monkeys[i].items)
+        
+        for item in monkeys[i].items[0:]:
+            
+            if i == 0:
+                monkeys[i].items[0]=int(monkeys[i].items[0])*19
+            elif i == 1:
+                monkeys[i].items[0]=int(monkeys[i].items[0])+6
+            elif i == 2:
+                monkeys[i].items[0]=int(monkeys[i].items[0])*int(monkeys[i].items[0])
+            elif i == 3:
+                monkeys[i].items[0]=int(monkeys[i].items[0])+3
+            elif i == 4:
+                monkeys[i].items[0]=int(monkeys[i].items[0])*17
+            elif i == 5:
+                monkeys[i].items[0]=int(monkeys[i].items[0])+3
+            elif i == 6:
+                monkeys[i].items[0]=int(monkeys[i].items[0])*int(monkeys[i].items[0])
+            elif i == 7:
+                monkeys[i].items[0]=int(monkeys[i].items[0])+6
 
 
+            
+            if int(monkeys[i].items[0])%monkeys[i].operation==0:
+                monkeys[monkeys[i].t].items.append(monkeys[i].items[0])
+            else:
+                monkeys[monkeys[i].f].items.append(monkeys[i].items[0])
+            monkeys[i].items.pop(0)
+    
+    for items in monkeys:
+        print("Round: ",n+1,"--",items.items)

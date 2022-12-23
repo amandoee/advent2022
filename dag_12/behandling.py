62abb27850f9a5a0eb0f07b121a0f1a965ec121c
amandoee <amandoee@gmail.com>
2022-12-23 16:13:38 +0100

    nisser

--- dag_12/behandling.py
+++ dag_12/behandling.py
@@ -13,10 +13,10 @@ def start_end_size():
             for letter in line:
                 if letter=='S':
                     start=[num,line.index(letter)]
-                    linje.append('S')
+                    linje.append(1)
                 elif letter=='E':
                     end=[num,line.index(letter)]
-                    linje.append('E')
+                    linje.append(26)
 
                 else:
                     linje.append(char_to_number(letter))
@@ -28,6 +28,54 @@ def start_end_size():
 
 start, end, height, width, kort = start_end_size()
 
-for item in kort:
-    print(item)
+travel_path=[]
+last_action_reverse=[]
+#function to move recursively
+def useless(start):
+    travel_path.append(start)
+    #left
+    if start[0]>0 and kort[start[1]][start[0]-1]!='.' and [start[0]-1,start[1]] not in travel_path and (kort[start[1]][start[0]]>= kort[start[1]][start[0]-1]-1):
+            last_action_reverse.append("right")    
+            useless([start[0]-1,start[1]])
+    
+    #up
+    elif start[1]>0 and kort[start[1]-1][start[0]]!='.' and [start[0],start[1]-1] not in travel_path and (kort[start[1]][start[0]]>= kort[start[1]-1][start[0]]-1): #MANGLER ÆNDRING
+            last_action_reverse.append("down")
+            useless([start[0],start[1]-1])
+    
+    #right
+    elif start[0]<width-1 and kort[start[1]][start[0]+1]!='.' and [start[0]+1,start[1]] not in travel_path and (kort[start[1]][start[0]]>= kort[start[1]][start[0]+1]-1):
+            last_action_reverse.append("left")
+            useless([start[0]+1,start[1]])
+    
+    #down
+    elif start[1]<height-1 and kort[start[1]+1][start[0]]!='.' and [start[0],start[1]+1] not in travel_path and (kort[start[1]][start[0]]>= kort[start[1]+1][start[0]]+1): #MANGLER ÆNDRING
+            last_action_reverse.append("up")
+            useless([start[0],start[1]+1])
+    
+    #If no more moves are possible, paint the path as useless
+    elif (start!=end):
+        kort[start[1]][start[0]]='.'
+        if (travel_path.pop()!=[0,0]):
+            travel_path.pop()
+        
+        action=last_action_reverse.pop()
+        #Nået hertil. Mangler backtracking funktion færddiggjort
+
+        if (action=="left"):
+            useless([start[0]-1,start[1]])
+        elif (action=="right"):
+            useless([start[0]+1,start[1]])
+        elif (action=="up"):
+            useless([start[0],start[1]-1])
+        elif (action=="down"):
+            useless([start[0],start[1]+1])
+
+
+useless(start)
+
+print(kort)
+
+#Tilføj op og ned kritierier for at måtte hoppe. så bør det virke.
+
     
